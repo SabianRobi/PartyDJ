b2a92b61de2c6ed0b254a7f80b182296c0d2e9ba
SabianRobi <sabian.robi@gmail.com>
2023-10-27 12:21:07 +0200

    Initialized Spring application

---
+++ src/main/java/com/partydj/partydj/PartyDjApplication.java
@@ -0,0 +1,12 @@
+package com.partydj.partydj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyDjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PartyDjApplication.class, args);
+    }
+}
---
+++ src/test/java/com/partydj/partydj/PartyDjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.partydj.partydj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyDjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
