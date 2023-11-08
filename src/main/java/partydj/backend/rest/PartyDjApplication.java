a5f9106a3f553ca327287cf2c6ffa33fa2184a6f
SabianRobi <sabian.robi@gmail.com>
2023-11-08 16:58:29 +0100

    Removed references for TestDataGenerator

--- src/main/java/partydj/backend/rest/PartyDjApplication.java
+++ src/main/java/partydj/backend/rest/PartyDjApplication.java
@@ -1,28 +1,11 @@
 package partydj.backend.rest;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import partydj.backend.rest.service.TestDataGenerator;
 
 @SpringBootApplication
 public class PartyDjApplication {
-
-    @Autowired
-    private TestDataGenerator testDataGenerator;
-
     public static void main(String[] args) {
         SpringApplication.run(PartyDjApplication.class, args);
     }
-
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
-            testDataGenerator.createTestData();
-        };
-    }
-
 }
