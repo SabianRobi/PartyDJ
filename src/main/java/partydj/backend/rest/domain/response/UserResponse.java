07837ee8ed8c8a62c64e4e899d7c3d350f9824df
SabianRobi <sabian.robi@gmail.com>
2023-11-08 17:01:57 +0100

    Reworked UserResponse model

--- src/main/java/partydj/backend/rest/domain/response/UserResponse.java
+++ src/main/java/partydj/backend/rest/domain/response/UserResponse.java
@@ -1,25 +1,17 @@
 package partydj.backend.rest.domain.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class UserResponse {
     private int id;
     private String email;
     private String username;
-//    private UserType userType;
-//    private PartyRole partyRole;
-//    private int spotifyCredentialId;
-//    private int partyId;
 // TODO: isSpotifyConnected should be added
-
-    @Builder
-    public UserResponse(int id, String email, String username) {
-        this.id = id;
-        this.email = email;
-        this.username = username;
-    }
 }
