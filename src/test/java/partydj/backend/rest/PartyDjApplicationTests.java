c0e8a577315bb8bd6e142201023800db7283ac84
SabianRobi <sabian.robi@gmail.com>
2023-12-04 15:57:22 +0100

    Added custom properties file for tests

--- src/test/java/partydj/backend/rest/PartyDjApplicationTests.java
+++ src/test/java/partydj/backend/rest/PartyDjApplicationTests.java
@@ -2,8 +2,10 @@ package partydj.backend.rest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 class PartyDjApplicationTests {
 
     @Test
